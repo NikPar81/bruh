b8e6ffbb1d037978ce78e71f3a4bebfc94f54b53
poperdimka <83829174+poperdimka@users.noreply.github.com>
2021-09-28 00:30:17 +0300

    прыг кота

--- Assets/Scripts/cmove.cs
+++ Assets/Scripts/cmove.cs
@@ -6,8 +6,15 @@ using UnityEngine.SceneManagement;
 public class cmove : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
     private float moveInput;
     private bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    private int extraJumps;
+    public int extraJumpsValue;
     public Animator anim;
     private Rigidbody2D rb;
 
@@ -15,12 +22,13 @@ public class cmove : MonoBehaviour
     public void Start()
     {
         PlayerPrefs.SetInt("catEnd", 0);
+        extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
     private void FixedUpdate()
     {
-
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
         moveInput = Input.GetAxis("Horizontal1");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
@@ -41,6 +49,25 @@ public class cmove : MonoBehaviour
         {
             anim.Play("CatStay");
         }
+    }
+    public void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+        if (Input.GetKey(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+
+
+        }
+        else if (Input.GetKey(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
     }
     void OnTriggerEnter2D(Collider2D catEnd)
     {
